c083634a1ccc56bba1dba73fba716fe8bdcf5ae3
Adriano Misina <adriano.misina@gmail.com>
2022-06-02 09:43:12 -0300

    paginacao

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="produtos-container">
     <div v-if="produtos && produtos.length" class="produtos">
-      <div class="produto" v-for="produto in produtos" :key="produto.id">
+      <div class="produto" v-for="(produto, index) in produtos" :key="index">
         <router-link to="/">
           <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
           <p class="preco">{{produto.preco}}</p>
@@ -9,6 +9,7 @@
           <p>{{produto.descricao}}</p>
         </router-link>
       </div>
+      <ProdutosPaginar :produtosTotal = "produtosTotal" :produtosPorPagina="produtosPorPagina"/>
     </div>
     <div v-else-if="produtos && produtos.length === 0">
       <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
@@ -17,16 +18,24 @@
 </template>
 
 <script>
+import ProdutosPaginar from "@/components/ProdutosPaginar.vue"
 import { api } from "@/services.js";
 import { serialize } from "@/helpers.js";
 
 export default {
+  name: "produtosLista-components",
+  components: {
+    ProdutosPaginar
+  },
+
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 9
+      produtosPorPagina: 3,
+      produtosTotal: 0
     };
   },
+
   computed: {
     url() {
       const query = serialize(this.$route.query);
@@ -36,6 +45,7 @@ export default {
   methods: {
     getProdutos() {
       api.get(this.url).then(response => {
+        this.produtosTotal = Number(response.headers["x-total-count"]);
         this.produtos = response.data;
       });
     }
